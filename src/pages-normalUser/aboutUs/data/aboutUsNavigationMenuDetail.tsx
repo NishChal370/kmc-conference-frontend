3e7c2f4f4a75c2583cc147e7eb8dc48f13aac19d
NishChal370 <nishchal.370@gmail.com>
2024-01-02 16:58:42 +0545

    Update organizer path  in about-us page

--- src/pages-normalUser/aboutUs/data/aboutUsNavigationMenuDetail.tsx
+++ src/pages-normalUser/aboutUs/data/aboutUsNavigationMenuDetail.tsx
@@ -1,4 +1,4 @@
-import { SCHEDULE_PATH, SPEAKER_PATH } from "@/constants/routePath/path-normalUser";
+import { ORGANIZERS_PATH, SCHEDULE_PATH, SPEAKER_PATH } from "@/constants/routePath/path-normalUser";
 
 export const ABOUT_US_NAVIGATION_DETAIL = [
       {
@@ -16,7 +16,7 @@ export const ABOUT_US_NAVIGATION_DETAIL = [
       {
             title: "Organizers",
             subTitle: "Explore your organizers",
-            path: "",
+            path: ORGANIZERS_PATH.organizer.full,
             img: "https://images.unsplash.com/photo-1676873779968-f991734295c9?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
 ] as const;
