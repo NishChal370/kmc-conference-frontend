761c5c2d730016de8b815351346f4771e62a451a
NishChal370 <nishchal.370@gmail.com>
2024-01-06 16:06:21 +0545

    Update PrivateRoute redirect

--- src/protectedRoute/PrivateRoute.tsx
+++ src/protectedRoute/PrivateRoute.tsx
@@ -2,16 +2,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAppSelector } from "@/app/hooks";
 import { verifyLoginState } from "./feature/verifyLoginSlice";
 import { Status, UserRole } from "@/enum/commonEnum";
-import { HOME_PATH } from "@/constants/routePath/path-normalUser";
 import getTokenDetail from "@/utils/token/getTokenDetail";
 
 function PrivateRoute() {
       const { status } = useAppSelector(verifyLoginState);
 
-      if (status === Status.FAILED) return <Navigate to={HOME_PATH.home.full} replace />;
+      if (status === Status.FAILED) return <Navigate to="/not-found" replace />;
 
       if (status === Status.SUCCEEDED && getTokenDetail.loggedInUserRole() === UserRole.PARTICIPANTS)
-            return <Navigate to={HOME_PATH.home.full} replace />;
+            return <Navigate to="/not-found" replace />;
 
       if (status === Status.SUCCEEDED) return <Outlet />;
 
