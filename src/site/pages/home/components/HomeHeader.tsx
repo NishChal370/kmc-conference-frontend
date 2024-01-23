91af06ce5aafd760b93675b3ba6d07173d15e8b5
NishChal370 <nishchal.370@gmail.com>
2024-01-23 22:51:13 +0545

    Update hero video

--- src/site/pages/home/components/HomeHeader.tsx
+++ src/site/pages/home/components/HomeHeader.tsx
@@ -1,21 +1,33 @@
 import heroVideo from "@/assets/video/hero-video.mp4";
+import smallHeroVideo from "@/assets/video/hero-video-2.mp4";
 import ExploreScheduleButton from "./ExploreScheduleButton";
+import useWindowSize from "@/site/hooks/windowsResize/useWindowSize";
 import HeaderAnimatedText from "@/template/animation/HeadingAnimatedText";
 import ScaleRaiseUpAnimationWrapper from "@/template/animation/ScaleRaiseUpAnimationWrapper";
 import "../style/homeHeader.css";
 
 function HomeHeader() {
+      const { width } = useWindowSize();
+
       return (
             <header
                   className="relative flex w-full px-0
-                        h-[40rem]
+                        h-[24rem]
+                        xs:h-[24rem]
+                        [@media(min-width:375px)]:h-[26rem]
+                        [@media(min-width:425px)]:h-[27rem]
+                        [@media(min-width:426px)]:h-[30rem]
+                        [@media(min-width:427px)]:h-[32rem]
+                        [@media(min-width:428px)]:h-[33rem]
+                        [@media(min-width:429px)]:h-[48rem]
+                        sm:h-[70rem]
                         lg:h-[36rem]
                         xl:h-[50rem]
                         [@media(min-width:2088px)]:h-[60rem]
                   "
             >
                   <video
-                        src={heroVideo}
+                        src={width < 768 ? smallHeroVideo : heroVideo}
                         autoPlay
                         muted
                         playsInline
@@ -25,7 +37,7 @@ function HomeHeader() {
                         Your browser does not support the video tag.
                   </video>
 
-                  <span className="header absolute flex flex-col justify-center items-start w-full  px-4 h-full [&>*:first-child]:lg:w-[70%] [&>*:first-child]:max-w-[76rem]">
+                  <span className="header absolute hidden md:flex flex-col justify-center items-start w-full  px-4 h-full [&>*:first-child]:lg:w-[70%] [&>*:first-child]:max-w-[76rem]">
                         <ScaleRaiseUpAnimationWrapper>
                               <article className="translate-y-40 sm:translate-y-32 xl:translate-y-40 3xl:translate-y-32 lg:translate-x-48 text-white font-bold text-2xl sm:text-5xl flex flex-col items-start justify-center gap-10">
                                     <span className="drop-shadow-2xl">
