1e10d6a3e603fa94d101470905921e55ddc5cd92
NishChal370 <nishchal.370@gmail.com>
2024-01-31 19:51:39 +0545

    add raise up animation in home know-about-conference section

--- src/site/pages/home/components/HomeKnowAboutConference.tsx
+++ src/site/pages/home/components/HomeKnowAboutConference.tsx
@@ -3,6 +3,7 @@ import AppIcon from "@/shared/icon/AppIcon";
 import HeaderAnimatedText from "@/template/animation/HeadingAnimatedText";
 import HOME_KNOW_ABOUT_CONFERENCE_DETAIL from "../data/homeKnowAboutConferenceDetail";
 import "../style/homeKnowAboutConference.css";
+import ScaleRaiseUpAnimationWrapper from "@/template/animation/ScaleRaiseUpAnimationWrapper";
 
 function HomeKnowAboutConference() {
       const navigate = useNavigate();
@@ -19,27 +20,36 @@ function HomeKnowAboutConference() {
                         className="font-bold text-4xl"
                   />
 
-                  <span className="grid lg:grid-cols-2 gap-14 w-full">
+                  <span
+                        className="grid md:grid-cols-2 gap-14 w-full
+                              md:gap-6 
+                              lg:gap-14
+                        "
+                  >
                         {HOME_KNOW_ABOUT_CONFERENCE_DETAIL.map((data) => (
-                              <div key={data.id} id={data.id} className="w-full h-[18rem] object-cover flex">
-                                    <span className="relative group trapezoid-wrapper cursor-pointer bg-black/70 w-full h-full text-white flex flex-col justify-between">
-                                          <div className="trapezoid-top"></div>
-                                          <div className="trapezoid-bottom"></div>
-                                          <article className="absolute flex flex-col translate-x-[12%] w-[50%] bottom-0 h-[24%] pb-4 group-hover:h-fit  ">
-                                                <p className="text-3xl font-semibold">{data.title}</p>
-                                                <p className="hidden group-hover:flex">{data.description}</p>
+                              <ScaleRaiseUpAnimationWrapper key={data.id}>
+                                    <div id={data.id} className="w-full h-[18rem] object-cover flex">
+                                          <span className="relative group trapezoid-wrapper cursor-pointer bg-black/70 w-full h-full text-white flex flex-col justify-between">
+                                                <div className="trapezoid-top"></div>
+                                                <div className="trapezoid-bottom"></div>
+                                                <article className="absolute flex flex-col translate-x-[12%] w-[50%] bottom-0 h-[24%] pb-4 group-hover:h-fit  ">
+                                                      <p className="text-3xl font-semibold">{data.title}</p>
+                                                      <p className="hidden group-hover:flex">
+                                                            {data.description}
+                                                      </p>
 
-                                                <button
-                                                      type="button"
-                                                      className="group w-fit hidden group-hover:flex items-center pt-2 font-semibold transition-transform duration-300 ease-in-out hover:gap-2"
-                                                      onClick={clickHandler(data.pathName)}
-                                                >
-                                                      Explore
-                                                      <AppIcon name="arrow-right" />
-                                                </button>
-                                          </article>
-                                    </span>
-                              </div>
+                                                      <button
+                                                            type="button"
+                                                            className="group w-fit hidden group-hover:flex items-center pt-2 font-semibold transition-transform duration-300 ease-in-out hover:gap-2"
+                                                            onClick={clickHandler(data.pathName)}
+                                                      >
+                                                            Explore
+                                                            <AppIcon name="arrow-right" />
+                                                      </button>
+                                                </article>
+                                          </span>
+                                    </div>
+                              </ScaleRaiseUpAnimationWrapper>
                         ))}
                   </span>
             </section>
