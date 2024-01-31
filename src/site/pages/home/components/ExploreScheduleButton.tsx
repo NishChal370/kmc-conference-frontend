3887cda20ba9e3b68b22d44ebca98dd2f63b6916
NishChal370 <nishchal.370@gmail.com>
2024-01-31 13:05:38 +0545

    Header design update
    add animation in explore button
    change header color to black

--- src/site/pages/home/components/ExploreScheduleButton.tsx
+++ src/site/pages/home/components/ExploreScheduleButton.tsx
@@ -10,15 +10,16 @@ function ExploreScheduleButton() {
       };
 
       return (
-            <Button
-                  onClickHandler={buttonHandler}
-                  variant="outlined"
-                  title="Explore"
-                  extraClassName="button-appear border border-white text-base !px-10 !py-1 font-medium text-white !w-fit !rounded-sm
-                        sm:text-xl
-                        active:text-primary active:border-primary
-                  "
-            ></Button>
+            <button type="button" className="button-appear animated-button" onClick={buttonHandler}>
+                  <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                  <span className="text">Explore</span>
+                  <span className="circle"></span>
+                  <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+            </button>
       );
 }
 
