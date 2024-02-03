244b95cc084373771eec8921260deec1bd6baafd
NishChal370 <nishchal.370@gmail.com>
2024-02-03 20:01:58 +0545

    Remove share button from session detail page

--- src/site/pages/scheduelDetail/components/scheduleActionHeader/ScheduleActionHeaderButton.tsx
+++ src/site/pages/scheduelDetail/components/scheduleActionHeader/ScheduleActionHeaderButton.tsx
@@ -5,13 +5,6 @@ interface IScheduleActionHeaderButton {
 function ScheduleActionHeaderButton({ allowedToAttend, attendButtonHandler }: IScheduleActionHeaderButton) {
       return (
             <aside className="flex gap-4 self-end">
-                  <button
-                        type="button"
-                        className="border border-primary rounded-md text-primary font-semibold tracking-wider w-full px-10 py-2"
-                  >
-                        Share
-                  </button>
-
                   {allowedToAttend && (
                         <button
                               type="button"
