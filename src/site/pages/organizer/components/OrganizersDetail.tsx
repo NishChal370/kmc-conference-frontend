6c0e469d12278813d3052670a87c07afc6f43531
NishChal370 <nishchal.370@gmail.com>
2024-02-03 23:11:41 +0545

    Update site organizer
    Remove NepalGov description from organizer pages
    
    Remove organizer from site home page

--- src/site/pages/organizer/components/OrganizersDetail.tsx
+++ src/site/pages/organizer/components/OrganizersDetail.tsx
@@ -1,34 +1,33 @@
-import { ORGANIZERS_DETAIL } from "../seed/organizersDetails";
+import { KMC_DESCRIPTION, ORGANIZERS_DETAIL } from "../seed/organizersDetails";
 
 function OrganizersDetail() {
       return (
             <span className="flex flex-col justify-center items-center gap-10 h-full w-full leading-loose text-justify">
-                  {ORGANIZERS_DETAIL.map(({ image, name, description }, index) => (
-                        <div
-                              key={index}
-                              className={`flex justify-center w-full h-full py-24 ${index % 2 && "bg-mute"}`}
-                        >
-                              <span
-                                    className="flex flex-col gap-6 h-full w-full px-6
+                  <div className="flex justify-center w-full h-full py-24 ">
+                        <span
+                              className="flex flex-col gap-6 h-full w-full px-6
                                           sm:px-10 
                                           md:gap-10 md:flex-row md:w-[70%] md:max-w-[80rem]
                                     "
-                              >
-                                    <section>
+                        >
+                              <section className="flex md:flex-col gap-10">
+                                    {ORGANIZERS_DETAIL.map(({ image, name }, index) => (
                                           <img
-                                                className="w-60 max-w-[15rem] min-w-[15rem] h-fit"
+                                                key={index}
+                                                title={name}
+                                                className="w-20 max-w-[5rem] md:w-60 md:max-w-[15rem] md:min-w-[15rem] h-fit"
                                                 src={image}
-                                                alt="organizer"
+                                                alt="kmcLogo"
                                           />
-                                    </section>
+                                    ))}
+                              </section>
 
-                                    <article className="flex flex-col gap-4 w-full h-full">
-                                          <h6 className="font-semibold text-xl">About {name}</h6>
-                                          <p>{description}</p>
-                                    </article>
-                              </span>
-                        </div>
-                  ))}
+                              <article className="flex flex-col gap-4 w-full h-full">
+                                    <h6 className="font-semibold text-xl">About {KMC_DESCRIPTION.name}</h6>
+                                    <p>{KMC_DESCRIPTION.description}</p>
+                              </article>
+                        </span>
+                  </div>
             </span>
       );
 }
--- src/site/pages/organizer/seed/organizersDetails.ts
+++ src/site/pages/organizer/seed/organizersDetails.ts
@@ -6,28 +6,22 @@ export const ORGANIZERS_DETAIL = [
 
             image: kmcLogo,
             name: "Kathmandu Metropolitan City",
-            description: `Kathmandu is the capital and eldest metropolitan city of Nepal. The city is the urban core of the Kathmandu Valley in the Himalayas, which also contains two sister cities namely Patan or Lalitpur, 5 kilometres (3.1 mi) to its southeast and Bhaktapur, 14 kilometres (8.7 mi) to its east. It is also acronymed as ‘KTM’.
-
-            The city stands at an elevation of approximately 1,400 metres (4,600 ft) in the bowl-shaped valley in central Nepal surrounded by four major mountains, namely: Shivapuri, Phulchowki, Nagarjun and Chandragiri. It is inhabited by 671,846 (2001) people. The Kathmandu valley with its three districts including Kathmandu District accounts for a population density of only 97 per square kilometres whereas Kathmandu metropolitan city has a density of 13,225 per square kilometres. It is by far the largest urban agglomerate in Nepal, accounting for 20% of the urban population in an area of 5,067 hectares (12,520 acres) (50.67 square kilometres (19.56 sq mi)).
-            
-            Kathmandu is not only the capital of the Federal Democratic Republic of Nepal but also the headquarters of the Central Region (Madhyamanchal) among the five development regions constituted by the 14 administrative zones of Nepal located at the central part of the country. The Central region has three zones namely, Bagmati, Narayani and Janakpur. Kathmandu is located in the Bagmati Zone. Kathmandu, as the gateway to Nepal Tourism, is the nerve centre of the country’s economy. With the most advanced infrastructure among urban areas in Nepal, Kathmandu’s economy is tourism centric accounting for 3.8% of the GDP in 1995-96 (had declined since then due to political unrest but has picked up again).
-            
-            The city’s rich history is nearly 2000 years old, as inferred from an inscription in the valley. Its religious affiliations are dominantly Hindu followed by Buddhism. People of other religious beliefs also live in Kathmandu giving it a cosmopolitan culture. Nepali is the common language of the city, though many speak the Nepal Bhasa Newari as it is the center of the Newar people and culture. English is understood by all of the educated population of the city. The literacy rate is 98% in the city.
-            
-            Kathmandu is now the premier cultural and economic hub of Nepal and is considered to have the most advanced infrastructure among urban areas in Nepal. From the point of view of tourism, economy and cultural heritage, the sister cities of Patan (lalitpur) and Bhaktapur are integral to Kathmandu.`,
       },
       {
 
             image: nepalGovLogo,
             name: "Nepal Government",
-            description: `The government of Nepal operates as a federal democratic republic. This political framework was established after the adoption of a new constitution in 2015, which marked a significant transition from a monarchy to a republic. The President of Nepal serves as the head of state, while the Prime Minister is the head of government, playing a more significant role in day-to-day governance.
-
-            The federal structure of Nepal is divided into seven provinces, each with its own local government, reflecting the country's commitment to decentralization and local governance. This structure is designed to address the diverse needs of its population, which is varied in terms of ethnicity, language, and culture.
-            
-            The political system in Nepal is characterized by a multi-party system, where numerous political parties compete for power. This diversity in political representation reflects the country's complex social fabric. However, this multiplicity of parties can also lead to political instability and frequent changes in government.
-            
-            Nepal's government faces various challenges, including economic development, infrastructure improvement, and addressing socio-economic disparities. The country is still recovering from the devastating earthquake in 2015, which significantly impacted its development. Furthermore, Nepal navigates a delicate balance in its foreign relations, particularly between its two large neighbors, India and China.
-            
-            In summary, Nepal's government is a federal democratic republic with a multi-party system, striving to address its diverse population's needs while facing challenges in development and international diplomacy.`
       },
 ]
+
+
+export const KMC_DESCRIPTION = {
+      name: "Kathmandu Metropolitan City",
+      description: `Kathmandu Metropolitan City, the capital of Nepal, stands as the vibrant heart of the nation, blending rich cultural heritage with the dynamism of modern urban life. With its historical temples, bustling streets, and diverse communities, Kathmandu serves as a melting pot of tradition and progress. 
+      In recent years, Kathmandu Metropolitan City has witnessed significant strides in its technical and IT landscape, marking a 
+      notable shift towards digitalization and innovation. Through initiatives aimed at enhancing connectivity, efficiency, and service delivery,
+       the city has embraced technology to streamline administrative processes, improve public services, and foster community engagement. From the implementation 
+       of digital platforms for citizen services to the integration of smart infrastructure solutions, Kathmandu Metropolitan City is leveraging technology to 
+       address urban challenges and empower its residents in the digital age. This evolution underscores the city's commitment to embracing innovation while 
+       preserving its cultural legacy, poised to meet the demands of a rapidly evolving urban landscape.`
+}
